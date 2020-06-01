a1d5e1d6617919f96f94b289dbac1ae172f42069
Stigyi <akak6@tlen.pl>
2020-06-01 12:59:34 +0200

    initial commit

---
+++ src/test/java/pl/skosiak/LogParser/LogParserApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.skosiak.LogParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
